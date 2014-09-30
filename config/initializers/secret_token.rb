635d4d056ac8f28d2f8b6626f395bbf301274023
Shaun Ford <shaun.deandre@yahoo.com>
2014-09-30 13:30:48 -0400

    initial commit with postgres db

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PocketChange::Application.config.secret_token = '15b14ccc60d3d9055f905184c7d8078c7c9bc0f6fc143d798e8a93ccbf98c15bc4f0967d4047c80b7395214e73b949e5018b81bd9fcd7046ad3b05af59132d0f'
