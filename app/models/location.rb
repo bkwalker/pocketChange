0d46aaef444cbd0500d23f7433e652de259d02cb
Dima <dyurkans@andrew.cmu.edu>
2014-10-05 14:51:42 -0400

    Basic model started for Location and Location_Address

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,28 @@
 class Location < ActiveRecord::Base
   attr_accessible :description, :item_id, :name, :user_id
+
+  # Relationships
+  has_one :location_address
+
+  # Callbacks
+  before_save :reformat_text
+
+  # Scopes
+  scope :alphabetical, order('name')
+
+  # Lists
+
+  # Validations
+  validates_numericality_of :item_id, :user_id
+  validates_presence_of :name, :description
+
+  # Other methods
+
+  # Private methods
+  private
+
+  def reformat_text
+    self.name = self.name.downcase.squish.titleize unless self.name.nil?
+  end
+
 end
