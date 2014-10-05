82fc1b232061c97915013c6d6d9b0dfc7eee06a0
Dima <dyurkans@andrew.cmu.edu>
2014-10-05 15:10:02 -0400

    added shoulda matchers for location_address

--- test/unit/location_address_test.rb
+++ test/unit/location_address_test.rb
@@ -1,7 +1,39 @@
 require 'test_helper'
 
 class LocationAddressTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	#Relationship Tests
+	should belong_to(:location)
+
+	# tests for street
+	should validate_presence_of(:street)
+
+	# tests for city
+	should validate_presence_of(:city)  
+
+	# tests for zip
+	should allow_value("15213").for(:zip)
+	should_not allow_value(nil).for(:zip)
+	should_not allow_value("bad").for(:zip)
+	should_not allow_value("1512").for(:zip)
+	should_not allow_value("152134").for(:zip)
+	should_not allow_value("15213-0983").for(:zip)
+
+	# tests for state
+	should allow_value("OH").for(:state)
+	should allow_value("PA").for(:state)
+	should allow_value("WV").for(:state)
+	should allow_value("NY").for(:state)
+	should allow_value("CA").for(:state)	
+	should_not allow_value("bad").for(:state)
+	should_not allow_value(10).for(:state)
+
+	#test location_id
+	should validate_numericality_of(:location_id)
+	should allow_value(3).for(:location_id)
+	should_not allow_value(3.14159).for(:location_id)
+	should_not allow_value(0).for(:location_id)
+	should_not allow_value(-1).for(:location_id)
+	should_not allow_value(nil).for(:location_id)
+
 end
