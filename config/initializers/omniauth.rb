c4f16c7ede4d138e9d36e61c1f3c718f2a62a6a9
Sophie Batton <sabatton@andrew.cmu.edu>
2014-10-28 14:58:39 -0400

    oauth seems to work

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '249587327081-gd5ud1u5krcb7jj56h38ht9d4kcu7fgl.apps.googleusercontent.com', 'ORas5O1QzWugxsxZiF5sgF5B', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  provider :google_oauth2, '249587327081-4up3p24k5sb0nt01hht00tuj9mtfoksv.apps.googleusercontent.com', 'J3YedAEG5Mjnljt5hL71E5wY', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
