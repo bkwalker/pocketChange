59e5283121d53417b5a205b91f9223171e66fbb7
Sophie Batton <sabatton@andrew.cmu.edu>
2014-10-28 13:00:23 -0400

    googleAuth initial steps

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,5 @@
+OmniAuth.config.logger = Rails.logger
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2, '249587327081-gd5ud1u5krcb7jj56h38ht9d4kcu7fgl.apps.googleusercontent.com', 'ORas5O1QzWugxsxZiF5sgF5B', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+end
