a66c90576f769c9f8e4eca60a5be3618d782b0c5
Dima <dyurkans@andrew.cmu.edu>
2014-10-28 13:11:18 -0400

    Completed steos from google auth tutorial. still not working

--- app/models/user.rb
+++ app/models/user.rb
@@ -60,6 +60,18 @@ class User < ActiveRecord::Base
     find_by_email(email).try(:authenticate, password)
   end
 
+  def self.from_omniauth(auth)
+    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.name = auth.info.name
+      user.oauth_token = auth.credentials.token
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.save!
+    end
+  end
+
+
   # Private methods
   private
 
