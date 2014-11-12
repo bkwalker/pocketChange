e0d85e6bccfb1f978715e05f52d866a1126f999d
Shaun Ford <shaun.deandre@yahoo.com>
2014-11-12 18:01:11 -0500

    authorization really working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
-  check_authorization
 
   rescue_from CanCan::AccessDenied do |exception|
     if current_user
