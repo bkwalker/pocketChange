a03b90ceea927a176491787bd105df32b518dd44
Shaun Ford <shaun.deandre@yahoo.com>
2014-11-19 18:06:00 -0500

    offer modal working; but submission is not

---
+++ app/assets/javascripts/myJS.js
@@ -0,0 +1,6 @@
+// $(document).ready(function () {
+//     $('.myOfferSubmit').click(function() {
+//         $('#myOfferSubmit').submit()
+//         return false;  // cancel the initial submit event
+//     });
+// });
